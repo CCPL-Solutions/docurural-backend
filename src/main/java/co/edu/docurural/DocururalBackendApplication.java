f62c9cee717e09538df208a3b617f94915a03a46
pedro-chavezcastro <pedro.chavezcastro@outlook.com>
2026-04-18 16:12:56 -0500

    first commit

---
+++ src/main/java/co/edu/docurural/DocururalBackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.docurural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocururalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocururalBackendApplication.class, args);
+	}
+
+}
